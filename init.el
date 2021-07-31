b30f61d4d14b9751d3b92b82b1b0332c42468b93
Mohammad Sheikh <mohammad@unit21.ai>
2021-07-31 13:55:56 -0400

    Add magit

--- init.el
+++ init.el
@@ -2,7 +2,7 @@
 (setq byte-compile-warnings '(cl-functions))
 (setq inhibit-startup-message t)
 (setq ispell-program-name "/usr/local/bin/aspell")
-(setq backup-directory-alist '(("." . "~/.emacs/saves")))
+(setq backup-directory-alist '(("." . "~/.emacs.d/saves")))
 
 ;; WIP
 ;; (global-set-key (kbd "C-v") 'View-scroll-half-page-up)
@@ -78,6 +78,20 @@
   (global-set-key [remap scroll-down-command] 'golden-ratio-scroll-screen-down)
   (global-set-key [remap scroll-up-command] 'golden-ratio-scroll-screen-up))
 
+;; use 0.618 proportion for new windows
+(use-package golden-ratio
+  :config (golden-ratio-mode 1))
+
+;; git interface
+(use-package magit)
+
+(setq telephone-line-lhs
+      '((accent . (telephone-line-vc-segment))))
+
+;; cleaner bottom info ribbon
+(use-package telephone-line
+  :config (telephone-line-mode 1))
+
 ;; minimap currently disabled
 ;; (minimap-mode)
 
@@ -121,7 +135,7 @@
  '(ansi-color-names-vector
    ["#454545" "#cd5542" "#6aaf50" "#baba36" "#5180b3" "#ab75c3" "#68a5e9" "#bdbdb3"])
  '(ansi-term-color-vector
-   [unspecified "#2d2a2e" "#ff6188" "#a9dc76" "#ffd866" "#78dce8" "#ab9df2" "#a1efe4" "#fcfcfa"])
+   [unspecified "#2d2a2e" "#ff6188" "#a9dc76" "#ffd866" "#78dce8" "#ab9df2" "#a1efe4" "#fcfcfa"] t)
  '(custom-safe-themes
    '("24168c7e083ca0bbc87c68d3139ef39f072488703dcdd82343b8cab71c0f62a7" "5185a285365a768a30ac274bdbc4437e7fd2fbe3107a1b0f2b60e900181905e0" default))
  '(minimap-dedicated-window nil)
@@ -131,7 +145,7 @@
  '(minimap-width-fraction 0.05)
  '(minimap-window-location 'right)
  '(package-selected-packages
-   '(golden-ratio-scroll-screen golden-ratoi-scroll-screen which-key use-package sublimity powerline monokai-pro-theme minimap jetbrains-darcula-theme ample-zen-theme ample-theme ace-window)))
+   '(golden-ratio magit telephone-line golden-ratio-scroll-screen golden-ratoi-scroll-screen which-key use-package sublimity powerline monokai-pro-theme minimap jetbrains-darcula-theme ample-zen-theme ample-theme ace-window)))
 
 ; LocalWords:  aspell monokai
 (custom-set-faces
